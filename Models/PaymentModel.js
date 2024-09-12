44c44eb2283549ff051b5ed059cbb526aa3b185f
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-09-12 21:49:55 +0300

    modeling

--- Models/PaymentModel.js
+++ Models/PaymentModel.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose')
 
 const PaymentSchema = mongoose.Schema({
-  
+  studentId: {
+    type: String,
+    required: true 
+  },
+  courseId: {
+    type: String,
+    required: true 
+  },
+  amount: {
+    type: Number,
+    required: true 
+  },
+  paymentDate: {
+    type: Date,
+    default: Date.now,
+    required: true 
+  },
+  paymentStatus: {
+    type: String,
+    required: true 
+  },
 }, { timestamps: true })
 
 const Payment = mongoose.model("Payment", PaymentSchema)
--- Models/RegistrationModel.js
+++ Models/RegistrationModel.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose')
 
 const RegistrationSchema = mongoose.Schema({
-  
+  studentId: {
+    type: String,
+    required: true 
+  },
+  courseId: {
+    type: String,
+    required: true 
+  },
+  registrationDate: {
+    type: Date,
+    default: Date.now,
+    required: true 
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["Paid", "NotPaid"],
+    default: "NotPaid",
+    required: true 
+  },
+  learningMode: {
+    type: String,
+    required: true 
+  },
 }, { timestamps: true })
 
 const Registration = mongoose.model("Registration", RegistrationSchema)
