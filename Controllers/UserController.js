b06f1a9aecc695f54a60234f214c7eee20276e2e
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-09-16 13:06:57 +0300

    Modeling

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,9 +1,3 @@
 const bcypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const User = require('../Models/UserModel')
-const Course = require('../Models/CourseModel')
-
-exports.Register = async (req, res) => {
-
-}
