81f8571b72b60c167eb5f99985f0ce7220451998
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-09-16 13:13:32 +0300

    Controller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,3 +1,10 @@
 const bcypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+exports.Register = async (req, res) => {
+  try {
+    const {name, gender, email, password, phone, role}
+  } catch (error) {
+    
+  }
+}
