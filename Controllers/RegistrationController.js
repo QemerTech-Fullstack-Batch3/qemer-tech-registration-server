48e5ce18b593e477ef9a1459229b6681d700ed75
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-09-17 13:39:34 +0300

    router-controller

--- Controllers/RegistrationController.js
+++ Controllers/RegistrationController.js
@@ -27,5 +27,26 @@ exports.Register = async (req, res) => {
 }
 
 exports.RegisterForCourse = async (req, res) => {
-  const {courseId, learningMode} = req.body
+  try {
+    const {studentInfo, courseId, learningMode} = req.body
+    const isCourseAvaiable = await Course.findOne({courseId: courseId}) 
+    if(!isCourseAvaiable){
+      return res.status().send("Course not found.")
+    }
+    
+    const existingStudent = await Register.findOne({courseId: courseId, studentInfo: studentInfo})
+    if(existingStudent){
+      return res.status(409).send("Student alreay registred for this course")
+    }
+  
+    const student = {
+      studentInfo,
+      courseId,
+      learningMode
+    }
+    await student.create()
+    res.status(201).send("Student successfully registered for the course!")
+  } catch (error) {
+    res.status(500).send("An error occurred while registering")
+  }
 }
