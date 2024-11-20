0b849288ee42f93d6af8e7f36d87ecb74b96518c
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-11-20 13:56:35 +0300

    morgan for logging

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
 const cors = require('cors')
 const connectDB = require('./Config/db')
 require("dotenv").config()
 const app = express()
+
+app.use(helmet())
+morgan.token('host', (req, res) => {
+  return req.hostname
+})
+morgan.token('param', (req, res, param) => {
+  return req.params[param]
+})
+app.use(morgan(':method :host :status :param[id] :res[content-length] - :response-time ms'));
+
 
 const PORT = process.env.PORT || 5000
 app.use(express.json())
