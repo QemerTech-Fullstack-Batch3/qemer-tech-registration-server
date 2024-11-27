4ce3ad600e06ab73194b5cde2abc9bc09d347366
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-11-27 15:05:34 +0300

    error handling and RBAC

--- Routers/CourseRouter.js
+++ Routers/CourseRouter.js
@@ -2,12 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const authenticateToken = require('../Middlewares/jwt_auth')
-<<<<<<< HEAD
 const {checkPermission} = require('../Middlewares/rbacMiddleware')
-
-=======
 const errorMiddleware = require('../Middlewares/errorMiddleware')
->>>>>>> Error-Handling-Middleware
 const {
   CreateCourse,
   GetCourses,
@@ -17,18 +13,10 @@ const {
 } = require('../Controllers/CourseController')
 const { route } = require('./RegistrationRouter')
 
-<<<<<<< HEAD
-router.post('/createcourse', authenticateToken, checkPermission('create_record'),CreateCourse)
-router.get('/getcourses', GetCourses)
-router.get('/getspecificcourse/:id', GetCourseInfo)
-router.patch('/editcourse/:id', authenticateToken, checkPermission('update_record'), EditCourse)
-router.patch('/updatestatus/:courseId', UpdateCourseStatus)
-=======
-router.post('/createcourse', authenticateToken, CreateCourse, errorMiddleware)
+router.post('/createcourse', authenticateToken, checkPermission('create_record'), CreateCourse, errorMiddleware)
 router.get('/getcourses', GetCourses, errorMiddleware)
 router.get('/getspecificcourse/:id', GetCourseInfo, errorMiddleware)
-router.patch('/editcourse/:id', authenticateToken, EditCourse, errorMiddleware)
+router.patch('/editcourse/:id', authenticateToken, checkPermission('update_record'), EditCourse, errorMiddleware)
 router.patch('/updatestatus/:courseId', UpdateCourseStatus, errorMiddleware)
->>>>>>> Error-Handling-Middleware
 
 module.exports = router 
